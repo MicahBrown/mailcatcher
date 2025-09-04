0369cd0ab013ef87a572782362db351e6b753691
Micah Brown <micahyb@gmail.com>
2025-09-04 11:29:33 -0500

    specify class for project association

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
   has_many :user_projects, class_name: "User::Project"
-  has_many :projects, through: :user_projects
+  has_many :projects, through: :user_projects, source: :project
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
--- app/models/user/project.rb
+++ app/models/user/project.rb
@@ -1,4 +1,4 @@
 class User::Project < ApplicationRecord
-  belongs_to :user
-  belongs_to :project
+  belongs_to :user, class_name: "::User"
+  belongs_to :project, class_name: "::Project"
 end
